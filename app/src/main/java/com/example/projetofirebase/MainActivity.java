a5825d68bbed03c219435f2beb82f6b83f6a50ac
SilvanaMenezes <silvanasales.menezes@gmail.com>
2024-03-21 00:15:23 -0300

    subindo todos os arquivos

---
+++ app/src/main/java/com/example/projetofirebase/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.projetofirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView text_tela_cadastro;
+    private EditText edit_email,edit_senha;
+    private Button bt_entrar;
+    private ProgressBar progressbar;
+    String[] mensagens = {"Preencha todos os campos"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if(email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v,mensagens[0],Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.YELLOW);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }else{
+                    AutenticarUsuario(v);
+                }
+            }
+        });
+
+    }
+
+    private void AutenticarUsuario(View view){
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    progressbar.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            TelaPrincipal();
+
+                        }
+                    }, 3000);
+                }else{
+
+                    String erro;
+                    try {
+                        throw task.getException();
+
+                    }catch (Exception e){
+                        erro = "Erro ao logar usuário";
+                    }
+
+                    Snackbar snackbar = Snackbar.make(view,erro,Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.YELLOW);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(usuarioAtual != null){
+            TelaPrincipal();
+        }
+    }
+
+    private void TelaPrincipal(){
+        Intent intent = new Intent(MainActivity.this,TelaPrincipal.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_entrar = findViewById(R.id.bt_entrar);
+        progressbar = findViewById(R.id.progressbar);
+
+    }
+}
